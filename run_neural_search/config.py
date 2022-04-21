fd94e5f6a30b2dae6f49798d5f79fad7f427bf72
AsmirMumin <asmir.muminovic@tenera.io>
2022-04-21 14:33:26 +0200

    get_data and cofnig for running the app

---
+++ run_neural_search/config.py
@@ -0,0 +1,5 @@
+DOCARRAY_PULL_NAME = "fashion-product-images-clip-all"
+DATA_DIR = "../data/images" 
+CSV_FILE = "../data/styles.csv" 
+WORKSPACE_DIR = "../embeddings"
+DIMS = 512 # This should be same shape as vector embedding
